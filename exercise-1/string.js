3c4d185922c7316e494adf828ac1a268e54136bd
Serigne Saliou SENE <serignesaliou.sene@outlook.fr>
2019-11-27 09:56:50 +0100

    Suite exercise 1

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -34,12 +34,36 @@ console.log(capitalize('rest  rezf'));
 
 function camelCase(str) {
     if (typeof str !== "string" || str.length === 0) return "";
-    const array = capitalize(str).split(" ");
+    let re = "/\s*(;|$)\s*\s*_/";
+    const array = str.split(re);
+    return array;
+}
+
+//console.log(camelCase("ToggleCase is_the coolesthello world"));
+
+function snake_case(str) {
+    if (typeof str !== "string" || str.length === 0) return "";
+    const array = str.toLowerCase().split(" ");
     return array.join("");
 }
 
-console.log(camelCase("hello world"));
+//console.log(snake_case("ToggleCase is_the coolest Hello World"));
 
-function snake_case(str) {
-    
+function leet(str) {
+  if (typeof str !== "string" || str.length === 0) return "";
+  str = str.replace('a','4');
+  str = str.replace('A','4');
+  str = str.replace('e','3');
+  str = str.replace('E','3');
+  str = str.replace('i','1');
+  str = str.replace('I','1');
+  str = str.replace('o','0');
+  str = str.replace('O','0');
+  str = str.replace('u','_');
+  str = str.replace('U','_');
+  str = str.replace('y','7');
+  str = str.replace('Y','7');
+  return str;
 }
+
+console.log(leet("TogglECase isu_theU Helylo WorYld"));
